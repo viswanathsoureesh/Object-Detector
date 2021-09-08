065e3094b1c354882840f817b4bcce225168306e
viswanathsoureesh <73836557+viswanathsoureesh@users.noreply.github.com>
2021-09-08 20:46:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+img="";
+
+function preload(){
+    img=loadImage("dog_cat.jpg")
+}
+
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.center();
+}
+
+function draw(){
+    image( img,0,0,500,500);
+    fill("#FF0000");
+    text("Dog",50,70);
+    noFill();
+    stroke("#FF0000");
+    rect(30,60,450,350);
+}
