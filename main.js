197c37b9de2a1e22f2b5263aedebe9ab70362d2b
viswanathsoureesh <73836557+viswanathsoureesh@users.noreply.github.com>
2021-09-15 13:19:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 img="";
-
+status="";
 function preload(){
     img=loadImage("download.jpg")
 }
@@ -7,6 +7,24 @@ function preload(){
 function setup(){
     canvas=createCanvas(500,500)
     canvas.center();
+
+    object_dectector=ml5.objectDetector('cocossd',modelLoaded) ;
+    document.getElementById("status").innerHTML='Status=Detecting Objects'
+}
+    
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+    object_dectector.detect(img,got_result);
+}
+
+function got_result(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+    }
 }
 
 function draw(){
@@ -16,4 +34,10 @@ function draw(){
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
+
+    fill("#FF0000");
+    text("Cat",250,130);
+    noFill();
+    stroke("#FF0000");
+    rect(230,100,450,350);
 }
